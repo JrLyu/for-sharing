30fe12bb51ca835ed9c36963769edff61b092314
Jiuru Lyu <lvjiuru@hotmail.com>
2023-09-07 22:34:08 -0400

    315 sept7 update

--- MATLAB/MATH 315/Lab01/ModifiedExpression.m
+++
@@ -1,5 +0,0 @@
-function f = ModifiedExpression(x)
-    syms t;
-    T = taylor(sin(t), t);
-    f = (x - subs(T, t, x)) / x^3;
-end
--- MATLAB/MATH 315/Lab01/VectorizedScaledNorm.m
+++
@@ -1,5 +0,0 @@
-function p = VectorizedScaledNorm(x)
-    c = max(x);
-    x = x ./ c;
-    p = c * sqrt(x(1)^2 + x(2)^2);
-end
