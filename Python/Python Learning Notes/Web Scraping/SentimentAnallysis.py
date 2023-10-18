b025973b3ee2c30516fd6470aa3d73d66a23aeef
Jiuru Lyu <lvjiuru@hotmail.com>
2023-10-17 22:14:21 -0400

    webscraper

---
+++ Python/Python Learning Notes/Web Scraping/SentimentAnallysis.py
@@ -0,0 +1,41 @@
+# Import modules
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import pandas as pd
+from tqdm import tqdm
+import SP500
+
+
+def sentiment(my_code):
+    """
+    This function calculates the sentiment score for a given stock ticker.
+        :param my_code: Stock ticker
+        :return: Sentiment score
+    """
+    # Import the dataset
+    df = pd.read_csv(f"data/{my_code}.csv")
+    analyzer = SentimentIntensityAnalyzer()
+    # Create a data set containing the sentiment scores
+    compound = []
+    for j in range(len(df)):
+        sentiment_dic = analyzer.polarity_scores(df.iloc[j, 1])
+        compound.append(sentiment_dic["compound"])
+    sentiment_score = pd.DataFrame({"Compound": compound})
+
+    return float(sentiment_score.mean())
+
+
+code = SP500.get_code()
+ticker = []
+sentimentScore = []
+for i in tqdm(range(len(code))):
+    sentimentScore.append(sentiment(code[i]))
+    ticker.append(code[i])
+
+sentiment_df = pd.DataFrame({"Ticker": ticker, "Sentiment Score": sentimentScore})
+sentiment_df.sort_values("Sentiment Score", ascending=False, inplace=True)
+
+# sentiment_df.dropna()
+
+print(f"Top 50 Stocks with Highest Sentiment Score: \n\n{sentiment_df.head(50)}")
+
+print(f"\n\n50 Stocks with Lowest Sentiment Score:\n\n{sentiment_df.dropna().tail(50)}")
--- Python/Python Learning Notes/Web Scraping/YahooFinanceScraper.py
+++ Python/Python Learning Notes/Web Scraping/YahooFinanceScraper.py
@@ -1,44 +1,41 @@
-#import modules
+# import modules
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from tqdm import tqdm
+import SP500
 
-# get the URL using response variable
-code = "JPM"
-# my_url = "https://finance.yahoo.com/news"
-my_url = f"https://finance.yahoo.com/quote/{code}?p={code}"
-response = requests.get(my_url)
-
-# Catching Exceptions
-print("response.ok : {} , response.status_code : {}".format(response.ok , response.status_code))
-print("Preview of response.text : ", response.text[:500])
 
 # utility function to download a webpage and return a beautiful soup doc
 def get_page(url):
     response = requests.get(url)
-    if not response.ok:
-        print('Status code:', response.status_code)
-        raise Exception('Failed to load page {}'.format(url))
     page_content = response.text
     doc = BeautifulSoup(page_content, 'html.parser')
     return doc
 
-# function call
-doc = get_page(my_url)
 
-# Appropriate tags common to news-headlines to filter out the necessary information.
-a_tags = doc.find_all('a', {'class': "js-content-viewer"})
-print(len(a_tags))
+# get the URL using response variable
+def getData(code):
+    """
+    This function scrapes the news headlines from Yahoo Finance for a given stock ticker.
+        :param code: Stock ticker
+        :return: A csv file containing the news headlines
+    """
+    my_url = f"https://finance.yahoo.com/quote/{code}?p={code}"
+    doc = get_page(my_url)
+    # Appropriate tags common to news-headlines to filter out the necessary information.
+    a_tags = doc.find_all('a', {'class': "js-content-viewer"})
+    news_list = []
+    for i in range(1, len(a_tags) + 1):
+        news = a_tags[i - 1].text
+        news_list.append(news)
 
-# Print(a_tags[1])
-news_list = []
+    # Save news headlines to a csv file for sentiment analysis
+    news_df = pd.DataFrame(news_list)
+    news_df.to_csv(f"data/{code}.csv")
 
-# Print top 10 Headlines
-for i in range(1, len(a_tags)+1):
-    news = a_tags[i-1].text
-    news_list.append(news)
-    print("Headline "+str(i)+ ": " + news)
 
-# Save news headlines to a csv file for sentiment analysis
-news_df = pd.DataFrame(news_list)
-news_df.to_csv('Market_News.csv')
+code = SP500.get_code()
+
+for i in tqdm(range(len(code))):
+    getData(code[i])
