8bfd628e15101135565cbd45975c96306eca0e41
Jiuru Lyu <lvjiuru@hotmail.com>
2024-02-08 15:16:09 -0500

    347/364/220 update

---
+++ Java/JavaDemo/Demo/src/Accumulation.java
@@ -0,0 +1,12 @@
+/**
+ * @author: Jiuru Lyu
+ * @createTime: 2024/02/07
+ * @description:
+ */
+
+public class Accumulation {
+    public static void main(String[] args) {
+        System.out.println(sumN(10));
+    }
+
+}
